ecc5b2087a3664dd03895bb8f2595a12a08c7a6e
celirojas <mcelinarojas@gmail.com>
2021-12-23 20:42:06 -0300

    Agrego header y footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Footer.css";
+
+function Footer(){
+    return (
+        <footer>
+            <ul>
+                <li><i className="fab fa-linkedin" /></li>
+                <li><i className="fab fa-github" /></li>
+                <li><i className="fas fa-envelope-square" /></li>
+            </ul>
+            <span>Celina Rojas 2021</span>  
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Navbar.css";
+
+function Navbar(){
+    return (
+        <nav>
+            <ul>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
