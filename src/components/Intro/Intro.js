ee58f016911a81d218a30ee633039fc99819f95e
Celina Rojas <81482627+celirojas@users.noreply.github.com>
2023-05-15 14:54:00 -0300

    Update Intro.js

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -8,8 +8,8 @@ function Intro(){
             <div className="intro-text">
                 <h1>Hi, I'm Celina Rojas</h1>
                 <h2>Web Developer</h2>
-                <p>I am a web developer and translator. I love learning about languages and technology. I am studying at Digital House to become a Certified Tech Developer.</p>
-                <a href="https://drive.google.com/file/d/1UTj3RpgDMJccM7CVV4MxTvsetezahMO0/view?usp=sharing" target="_blank" rel='noreferrer'>Download Resume</a>
+                <p>I am a web developer and translator. I love learning about languages, technology and design. I am a Certified Tech Developer.</p>
+        {/*<a href="https://drive.google.com/file/d/1UTj3RpgDMJccM7CVV4MxTvsetezahMO0/view?usp=sharing" target="_blank" rel='noreferrer'>Download Resume</a>*/}
             </div>
             <div className="intro-img">
                 <img src="celiRojas.jpg" alt="profile" />
